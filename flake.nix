832078216c48599caad8a0158dbd96822c06a783
hugosenari <hugosenari@gmail.com>
2022-03-12 02:46:12 -0300

    chore: re index rsvg

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper for librsvg, a Scalable Vector Graphics (SVG) rendering library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."rsvg-master".url = "path:./master";
+  inputs."rsvg-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."rsvg-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
